f8a99d90398dfc3cf4a8c513ace05da19c0b65f8
lowji194 <theloi194@gmail.com>
2023-11-23 09:12:16 +0700

    Update VLAN.sh

--- VLAN.sh
+++ VLAN.sh
@@ -81,8 +81,4 @@ gen_ifconfig > "${WORKDIR}/boot_ifconfig.sh"
 gen_proxy > "/usr/local/etc/LowjiConfig/UserProxy.cfg"
 echo "$IP6" > "${WORKDIR}/ip6.txt"
 
-if pgrep StartProxy >/dev/null; then
-  echo "LowjiProxy đang chạy, khởi động lại..."
-  /usr/bin/kill $(pgrep StartProxy)
-fi
 bash /home/Lowji194/boot_ifconfig.sh 2>/dev/null && ulimit -n 1000048 && /usr/local/etc/LowjiConfig/bin/StartProxy /usr/local/etc/LowjiConfig/UserProxy.cfg
